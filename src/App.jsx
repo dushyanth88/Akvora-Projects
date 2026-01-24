26c739a998d932410900fe0f98b91e7b07f14354
dushyanth88 <duhsyanthd912@gmail.com>
2026-01-24 08:42:21 +0530

    phone required

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ const App = () => {
           project_name: selectedProject.title,
           name: formData.name,
           email: formData.email,
-          phone: formData.phone || 'N/A',
+          phone: formData.phone,
           message: formData.message,
           botcheck: "" // Honeypot
         })
